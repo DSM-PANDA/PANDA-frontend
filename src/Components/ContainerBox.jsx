aeff101d6b7042fb38d2f6ebf3e6be70748687fb
waterbeen0530 <hange253@gmail.com>
2022-05-10 23:46:03 +0900

    [CREATE] New Main Page
    메인 페이지 생성

---
+++ src/Components/ContainerBox.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = styled.div`
+  position: absolute;
+  width: 540px;
+  height: 650px;
+  border-radius: 25px;
+  background: linear-gradient(180deg, #ffffff 65.1%, #eceaea 100%);
+  box-shadow: 10px 10px 4px rgba(0, 0, 0, 0.25);
+  /* z-index: -2; */
+`;
+
+function ContainerBox({ children }) {
+  return (
+    <>
+      <Section>
+        <>{children}</>
+      </Section>
+    </>
+  );
+}
+
+export default ContainerBox;
--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,25 +1,76 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Bamboo from "../img/Bamboo.png";
+import Circle1 from "../img/circle1.png";
+import Circle2 from "../img/circle2.png";
+import ContainerBox from "./ContainerBox";
 
 const Container = styled.div`
+  position: relative;
   width: 100vw;
-  height: 100hw;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   background: #f4fef9;
+  #Circle1 {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  #Circle2 {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+  }
 `;
 
-const Section = styled.div`
-  width: 665px;
-  height: 896px;
-  background: #fff;
+const Elements = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  h1 {
+    font-size: 50px;
+  }
+
+  img {
+    width: 420px;
+    height: 420px;
+  }
+
+  button {
+    width: 390px;
+    height: 60px;
+    border-radius: 25px;
+    border: none;
+    background-color: #639a55;
+    color: #fff;
+    font-size: 30px;
+  }
+
+  button:hover {
+    background: linear-gradient(126.86deg, #2ea849 29.75%, #82c970 71.43%);
+  }
 `;
 
 function MainPage() {
   return (
     <Container>
-      <Section></Section>
+      <img id="Circle1" src={Circle1} alt="" />
+      <img id="Circle2" src={Circle2} alt="" />
+      <>
+        <ContainerBox>
+          <Elements>
+            <h1>DSM-PANDA</h1>
+            <img src={Bamboo} alt="" />
+            <Link to="/login">
+              <button>시작하기</button>
+            </Link>
+          </Elements>
+        </ContainerBox>
+      </>
     </Container>
   );
 }
