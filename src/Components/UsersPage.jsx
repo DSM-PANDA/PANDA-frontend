297f892405efcab8a5a52bcd2b85ae51fdfe978e
waterbeen0530 <hange253@gmail.com>
2022-05-12 23:34:39 +0900

    [CREATE] New User Page
    비로그인 사용자 페이지 생성

---
+++ src/Components/UsersPage.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import styled from "styled-components";
+import ContainerBox from "./ContainerBox";
+import Bamboo from "../img/Bamboo.png";
+import Circle1 from "../img/UsersCircle1.png";
+import Circle2 from "../img/UsersCircle2.png";
+
+const Container = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #f4fef9;
+  z-index: 1;
+
+  #Circle1 {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 541px;
+    height: 591px;
+  }
+
+  #Circle2 {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 523px;
+    height: 647px;
+  }
+`;
+
+const Elements = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  div {
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  h1 {
+    font-size: 30px;
+    padding: 10px 50px 0;
+    margin-bottom: 0;
+  }
+
+  img {
+    width: 430px;
+    height: 430px;
+  }
+`;
+
+const Button = styled.div`
+  z-index: 10;
+  position: absolute;
+  bottom: 50px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  button {
+    margin-top: 20px;
+    width: 390px;
+    height: 55px;
+    border-radius: 21px;
+    border: none;
+    color: #fff;
+    font-size: 20px;
+  }
+
+  #Memobtn1 {
+    bottom: 0px;
+    background: linear-gradient(126.86deg, #2ea849 29.75%, #82c970 71.43%);
+  }
+
+  #Memobtn2 {
+    bottom: 0px;
+    background: #639a55;
+    transition: 0.3s;
+  }
+
+  #Memobtn2:hover {
+    opacity: 0;
+  }
+
+  #SingupBtn1 {
+    background: linear-gradient(126.86deg, #2ea849 29.75%, #82c970 71.43%);
+  }
+
+  #SingupBtn2 {
+    background: #639a55;
+    transition: 0.3s;
+  }
+
+  #SingupBtn2:hover {
+    opacity: 0;
+  }
+`;
+
+function UsersPage() {
+  return (
+    <Container>
+      <img id="Circle1" src={Circle1} alt="" />
+      <img id="Circle2" src={Circle2} alt="" />
+      <ContainerBox>
+        <Elements>
+          <div>
+            <h1>
+              User님의 대나무에
+              <br /> 쪽지가 0개 달려있습니다.
+            </h1>
+          </div>
+          <img src={Bamboo} alt="" />
+          <Button>
+            <button id="MemoBtn1" type="submit">
+              쪽지 달아주러 가기
+            </button>
+            <button id="MemoBtn2" type="submit">
+              쪽지 달아주러 가기
+            </button>
+            <button id="SingupBtn1">내 대나무 만들러 가기</button>
+            <button id="SingupBtn2">내 대나무 만들러 가기</button>
+          </Button>
+        </Elements>
+      </ContainerBox>
+    </Container>
+  );
+}
+
+export default UsersPage;
