9e7ac91bf99aba26a0c5ba1ebdeed92fcc8e32ec
waterbeen0530 <hange253@gmail.com>
2022-07-03 00:41:24 +0900

    [UPDATE] LoginPage  WebPublishing
    로그인페이지 퍼블리싱 완료

--- src/Components/LoginPage/LoginItem.jsx
+++ src/Components/LoginPage/LoginItem.jsx
@@ -2,18 +2,73 @@ import React from "react";
 import styled from "styled-components";
 import styles from "../../styles/font.module.css";
 
-const Container = styled.div`
+const Container = styled.form`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
   h1 {
     text-align: center;
     font-size: 70px;
-    margin: 40px 0 0;
+    margin: 35px 0 0;
+  }
+`;
+
+const InputElements = styled.div`
+  z-index: 10;
+  position: relative;
+  width: 390px;
+  height: auto;
+  margin-top: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  input {
+    position: absolute;
+    bottom: 0px;
+    width: 100%;
+    height: 35px;
+    margin: 0;
+    padding: 0 0 0 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: transparent;
+    border: none;
+    border-bottom: 2px solid #608182;
+    font-size: 20px;
+    font-weight: 500;
+    color: #000;
+    outline: none;
+    cursor: pointer;
+  }
+
+  label {
+    font-size: 25px;
+    font-weight: 600;
+    color: #505050;
+    transition: all 0.5s ease;
+  }
+
+  .LoginInput {
+    position: relative;
+    width: 100%;
+    height: 100px;
+  }
+
+  .LoginInput:nth-child(2) {
+    margin-top: 70px;
   }
 `;
 
 const ButttonElements = styled.div`
+  z-index: 10;
   position: absolute;
   bottom: 45px;
   left: 75px;
+  width: auto;
 
   button {
     width: 390px;
@@ -35,52 +90,18 @@ const ButttonElements = styled.div`
   }
 `;
 
-const InputElements = styled.div`
-  position: relative;
-  width: 400px;
-  height: 400px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  border: solid 1px red;
-
-  input {
-    width: 390px;
-    height: 35px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: transparent;
-    border: none;
-    border-bottom: 2px solid #608182;
-    font-size: 20px;
-    font-weight: 500;
-    color: #000;
-    outline: none;
-  }
-
-  label {
-    position: absolute;
-    left: 0px;
-    top: 15px;
-    font-size: 25px;
-    font-weight: 600;
-    color: #505050;
-    transition: all 0.5s ease;
-  }
-  #Login_id {
-  }
-`;
-
 export default function LoginItem() {
   return (
     <Container>
       <h1>Login</h1>
       <InputElements>
-        <div id="Login_Id">
-          <input type="text" placeholder="ID를 입력해주세요." />
-          <label for="id">ID</label>
+        <div className="LoginInput">
+          <label>ID</label>
+          <input type="text" placeholder="ID를 입력해주세요." required />
+        </div>
+        <div className="LoginInput">
+          <label>PW</label>
+          <input type="text" placeholder="PW를 입력해주세요." required />
         </div>
       </InputElements>
       <ButttonElements>
