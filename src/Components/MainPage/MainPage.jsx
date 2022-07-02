4e1d5e253ae332a3af6d14686e9440659a2b8f45
waterbeen0530 <hange253@gmail.com>
2022-07-03 00:43:19 +0900

    [CHANGE] File name change
    파일 이름 수정함

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Background from "../MainPage/Background";
-import ContainerBox from "./ContainerBox";
+import ContainerBox from "./MainContainer";
 import MainItem from "./MainItem";
 
 export default function MainPage() {
